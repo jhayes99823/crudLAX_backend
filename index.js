b38c60b88f6d8fd5d7ca00c71de914f6df225d39
Jordan Hayes <hayesja99@gmail.com>
2020-01-30 03:31:03 -0500

    add create coach sproc run and start setup for password salt and hashing

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const PORT = 3000;
+const PORT = 5000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use("/api", routes);
 
