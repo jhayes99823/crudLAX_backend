74a1f767bc6a8d3c24d4a82ead02ffc7c6f33c19
Jordan Hayes <hayesja99@gmail.com>
2020-02-07 14:12:59 -0500

    uncomment stat

--- routes/stat.routes.js
+++ routes/stat.routes.js
@@ -3,21 +3,23 @@ var router = express.Router();
 
 const { poolPromise, sql } =require('../db')
 
-// router.post('/stat/create-stat', async(req, res, next) => {
-//     console.log(req);
-//     const pool = await poolPromise;
-//     const result = await pool.request()
-//                     .input('opponent', sql.VarChar(30), req.body.opp)
-//                     .input('startTime', sql.DateTime, req.body.starttime)
-//                     .input('TID', sql.Int, req.body.tid)
-//                     .output('GID', sql.Int)
-//                     .query('SELECT [dbo].[fn_getGameFromOpponentAndDate] (@opponent, @startTime, @TID)')
+router.get('/stat/create-stat', async(req, res, next) => {
+    console.log(req);
+    const pool = await poolPromise;
+    const result = await pool.request()
+                    .input('opponent', sql.VarChar(30), req.query.opp)
+                    .input('startTime', sql.DateTime, req.query.starttime)
+                    .input('TID', sql.Int, req.query.tid)
+                    .output('GID', sql.Int)
+                    .query('SELECT [dbo].[fn_getGameFromOpponentAndDate] (@opponent, @startTime, @TID)')
     
-//     if (result.output.GID !== null) {
-//         res.end(JSON.stringify({ res: 'printing something', result: result }))
-
-//     }
-
-// })
+                    // res.end(JSON.stringify({ result: result }))
+    if (result.output.GID !== null) {
+        res.end(JSON.stringify({ result: result }))
+    }
+    else {
+        res.end(JSON.stringify({ message: 'Game Not Found'}))
+    }
+})
 
 module.exports = router;
