803f8f3f22838a13abdca3f020ea6f6e05a46ad5
Jordan Hayes <hayesja99@gmail.com>
2020-02-06 23:16:13 -0500

    comment out for small test

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -14,7 +14,13 @@ router.post('/team/create-team', async(req, res, next) => {
                     .input('schoolname', sql.VarChar(40), team.schoolname)
                     .input('state', sql.Char(2), team.state)
                     .execute('createTeam');
-})
+});
+
+// router.post('/team/update', async(req, res, next) => {
+//     const pool = await poolPromise;
+//     const result = await pool.request()
+//                         .input()
+// });
 
 router.get('/teams', async(req, res, next) => {
     const pool = await poolPromise;
