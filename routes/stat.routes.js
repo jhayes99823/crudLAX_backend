0f78bc370839c1fe13399c34b8e00eda7ef21efe
Joshua Warning <jdan.warning@gmail.com>
2020-02-11 21:46:35 -0500

    Im a genius?

--- routes/stat.routes.js
+++ routes/stat.routes.js
@@ -7,16 +7,39 @@ router.get('/stat/create-stat', async(req, res, next) => {
     const stat = req.body.stat;
     const pool = await poolPromise;
     const gameID = await pool.request()
-                    .input('opponent', sql.VarChar(30), 'Tigers')
-                    .input('startTime', sql.DateTime, '')
+                    .input('opponent', sql.VarChar(30), req.query.opponent)
+                    .input('startTime', sql.DateTime, req.query.startTime)
+                    .output('gameID', sql.int)
                     .query('SELECT from [dbo].[fn_getGameFromOpponentAndDate] (@opponent, @startTime)');
-    console.log(gameID);
+                    //errorcheck
+                    console.log(gameID);
     const PID = await pool.request()
-                    .input('playerUsername', sql.VarChar(30), 'louise')
+                    .input('playerUsername', sql.VarChar(30), req.query.Username)
+                    .output('PID', sql.int)
                     .query('SELECT * from [dbo].[getUserByUsername] (@Username)');
+                    //errorcheck
     console.log(PID);
-    // const result = await pool.request()
-    //                 .input(gameID, sql.Varchar(30), stat)
+    const result = await pool.request()
+                    .input('PID', sql.Int, PID.output.PID)
+                    .input('GID', sql.Int, gameID.output.GameID)
+                    .execute('insertStatForPlayerGame')
+                    //errorcheck
+    
+    const insertedStat = await pool.request()
+                    .input('forcedTurnover', sql.Int, req.query.forcedTurnover)
+                    .input('goals', sql.Int, req.query.goals)
+                    .input('faceoffSuccess', sql.Decimal(5,2), req.query.faceOffSuccess)
+                    .input('totalFaceoff', sql.Decimal(5,2), req.query.totalFaceoff)
+                    .input('assists', sql.Int, req.query.assists)
+                    .input('passSuccess', Decimal(5,2), req.query.passSuccess)
+                    .input('totalPass', sql.Decimal(5,2), req.query.totalPass)
+                    .input('groundBall', sql.Int, req.query.groundBall)
+                    .input('saves', sql.Int, req.query.saves)
+                    .input('PID', sql.Int, PID.output.PID)
+                    .input('GID', sql.Int, GameID.output.GameID)
+                    .execute('updateStatForPlayer')
+                    //errorcheck
+
     /*
     We need to create a stat for player using game id and player ID
     Then take the values from the user form and update created stat with said values
