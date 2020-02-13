d54f35ebf504d8c3179a80fcc084154d3b6c6de1
Joshua Warning <jdan.warning@gmail.com>
2020-02-13 12:13:51 -0500

    prepared statements attempt

--- routes/stat.routes.js
+++ routes/stat.routes.js
@@ -4,40 +4,53 @@ var router = express.Router();
 const { poolPromise, sql } =require('../db')
 
 router.get('/stat/create-stat', async(req, res, next) => {
-    const stat = req.body.stat;
+    // const stat = req.body.stat;
     const pool = await poolPromise;
-    const gameID = await pool.request()
-                    .input('opponent', sql.VarChar(30), req.query.opponent)
-                    .input('startTime', sql.DateTime, req.query.startTime)
-                    .output('gameID', sql.int)
-                    .query('SELECT from [dbo].[fn_getGameFromOpponentAndDate] (@opponent, @startTime)');
-                    //errorcheck
-                    console.log(gameID);
-    const PID = await pool.request()
-                    .input('playerUsername', sql.VarChar(30), req.query.Username)
-                    .output('PID', sql.int)
-                    .query('SELECT * from [dbo].[getUserByUsername] (@Username)');
-                    //errorcheck
-    console.log(PID);
-    const result = await pool.request()
-                    .input('PID', sql.Int, PID.output.PID)
-                    .input('GID', sql.Int, gameID.output.GameID)
-                    .execute('insertStatForPlayerGame')
-                    //errorcheck
+    console.log('parms', req.query);
+    var ps = new sql.PreparedStatement();
+        ps.input('opponent', sql.VarChar(30))
+        ps.input('startTime', sql.DateTime)
+        ps.prepare('SELECT [dbo].[fn_getGameFromOpponentAndDate] (@opponent, @startTime)', function(err) {
+            //errorchecks
+            ps.execute({opponent: req.query.opponent, startTime: req.query.startTime}, function(err, recordsets) {
+                //errorchecks
+                console.log(recordset[0].value);
+            }
+        }
+
+
+    // const gameID = await pool.request()
+    //                 .input('opponent', sql.VarChar(30), req.query.opponent)
+    //                 .input('startTime', sql.DateTime, req.query.startTime)
+    //                 .output('GID', sql.Int)
+    //                 .query()
+    //                 //errorcheck
+    //                 console.log(gameID);
+    // const PID = await pool.request()
+    //                 .input('playerUsername', sql.VarChar(30), req.query.Username)
+    //                 .output('PID', sql.int)
+    //                 .query('SELECT * from [dbo].[getUserByUsername] (@Username)');
+    //                 //errorcheck
+    // console.log(PID);
+    // const result = await pool.request()
+    //                 .input('PID', sql.Int, PID.output.PID)
+    //                 .input('GID', sql.Int, gameID.output.GameID)
+    //                 .execute('insertStatForPlayerGame')
+    //                 //errorcheck
     
-    const insertedStat = await pool.request()
-                    .input('forcedTurnover', sql.Int, req.query.forcedTurnover)
-                    .input('goals', sql.Int, req.query.goals)
-                    .input('faceoffSuccess', sql.Decimal(5,2), req.query.faceOffSuccess)
-                    .input('totalFaceoff', sql.Decimal(5,2), req.query.totalFaceoff)
-                    .input('assists', sql.Int, req.query.assists)
-                    .input('passSuccess', Decimal(5,2), req.query.passSuccess)
-                    .input('totalPass', sql.Decimal(5,2), req.query.totalPass)
-                    .input('groundBall', sql.Int, req.query.groundBall)
-                    .input('saves', sql.Int, req.query.saves)
-                    .input('PID', sql.Int, PID.output.PID)
-                    .input('GID', sql.Int, GameID.output.GameID)
-                    .execute('updateStatForPlayer')
+    // const insertedStat = await pool.request()
+    //                 .input('forcedTurnover', sql.Int, req.query.forcedTurnover)
+    //                 .input('goals', sql.Int, req.query.goals)
+    //                 .input('faceoffSuccess', sql.Decimal(5,2), req.query.faceOffSuccess)
+    //                 .input('totalFaceoff', sql.Decimal(5,2), req.query.totalFaceoff)
+    //                 .input('assists', sql.Int, req.query.assists)
+    //                 .input('passSuccess', Decimal(5,2), req.query.passSuccess)
+    //                 .input('totalPass', sql.Decimal(5,2), req.query.totalPass)
+    //                 .input('groundBall', sql.Int, req.query.groundBall)
+    //                 .input('saves', sql.Int, req.query.saves)
+    //                 .input('PID', sql.Int, PID.output.PID)
+    //                 .input('GID', sql.Int, GameID.output.GameID)
+    //                 .execute('updateStatForPlayer')
                     //errorcheck
 
     /*
