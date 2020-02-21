dc86d022005f6a1bbc22136850bf9813c94c3f7a
Joshua Warning <jdan.warning@gmail.com>
2020-02-20 21:24:57 -0500

    Edited routes further

--- routes/stat.routes.js
+++ routes/stat.routes.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 const { poolPromise, sql } =require('../db')
 
-router.post('/stat/create', async(req, res, next) => {
-    // const stat = req.body.stat;
-    const pool = await poolPromise;
-    console.log('parms', req.query);
-    const result = await pool.request()
-        .input('forcedTurnover', sql.Int, req.body.forcedTurnover)
-        .input('goals', sql.Int, req.body.goals)
-        .input('faceoffSuccess', sql.Decimal(5,2), req.body.faceOffSuccess)
-        .input('totalFaceoff', sql.Decimal(5,2), req.body.totalFaceoff)
-        .input('assists', sql.Int, req.body.assists)
-        .input('passSuccess', sql.Decimal(5,2), req.body.passSuccess)
-        .input('totalPass', sql.Decimal(5,2), req.body.totalPass)
-        .input('groundBall', sql.Int, req.body.groundBall)
-        .input('saves', sql.Int, req.body.saves)
-        .input('PID', sql.Int, req.body.PID)
-        .input('GID', sql.Int, req.body.GameID)
-        .execute('insertStatForPlayerGame')
+    router.post('/stat/create', async(req, res, next) => {
+        // const stat = req.body.stat;
+        const pool = await poolPromise;
+        console.log('parms', req.query);
+        const result = await pool.request()
+            .input('forcedTurnover', sql.Int, req.body.forcedTurnover)
+            .input('goals', sql.Int, req.body.goals)
+            .input('faceoffSuccess', sql.Decimal(5,2), req.body.faceOffSuccess)
+            .input('totalFaceoff', sql.Decimal(5,2), req.body.totalFaceoff)
+            .input('assists', sql.Int, req.body.assists)
+            .input('passSuccess', sql.Decimal(5,2), req.body.passSuccess)
+            .input('totalPass', sql.Decimal(5,2), req.body.totalPass)
+            .input('groundBall', sql.Int, req.body.groundBall)
+            .input('saves', sql.Int, req.body.saves)
+            .input('PID', sql.Int, req.body.PID)
+            .input('GID', sql.Int, req.body.GameID)
+            .execute('insertStatForPlayerGame')
 
-        console.log(result);
-        if (result.returnValue > 0) {
-            res.end(JSON.stringify({ success: true, stats: result.recordset }))
-        } else {
-            res.end(JSON.stringify({ success: false, ErrorCode: result.returnValue }))
-        }
-    
-    })
+            console.log(result);
+            if (result.returnValue > 0) {
+                res.end(JSON.stringify({ success: true, stats: result.recordset }))
+            } else {
+                res.end(JSON.stringify({ success: false, ErrorCode: result.returnValue }))
+            }
+        
+        })
 
     router.post('/stat/update', async(req, res, next) => {
         const pool = await poolPromise;    
